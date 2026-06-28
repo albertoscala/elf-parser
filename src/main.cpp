04d47bde4de2c9543dcd994964ff24c37ec265df
Alberto Scala <alberto.scala.it@gmail.com>
2026-06-28 22:48:43 +0300

    added all the tests for the header parsing

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,23 @@
 
 int main()
 {
-    elf::ELF elf("tests/minimal/test_static");
+    // Minimal test
+    elf::ELF elf_minimal_test("tests/minimal/test_static");
+    std::cout << elf_minimal_test.header << "\n";
 
-    std::cout << elf.header << "\n";
+    // Minimal test
+    elf::ELF elf_obj_file("tests/obj_file/test_obj.o");
+    std::cout << elf_obj_file.header << "\n";
+
+    // Minimal test
+    elf::ELF elf_shared_lib("tests/shared_lib/test_lib.so");
+    std::cout << elf_shared_lib.header << "\n";
+
+    // Minimal test
+    elf::ELF elf_stripped_binary("tests/stripped_binary/test_stripped");
+    std::cout << elf_stripped_binary.header << "\n";
+
+    // Minimal test
+    elf::ELF elf_with_debug("tests/with_debug/test_debug");
+    std::cout << elf_with_debug.header << "\n";
 }
